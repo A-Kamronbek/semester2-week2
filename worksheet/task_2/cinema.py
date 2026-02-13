bc0489bd08b64e56c5f833bd47e9bf6d3cc28a66
Kamronbek <118995322+A-Kamronbek@users.noreply.github.com>
2026-02-13 15:36:09 +0000

    cinema

--- worksheet/task_2/cinema.py
+++ worksheet/task_2/cinema.py
@@ -8,7 +8,7 @@ Please do not add any additional code underneath these functions.
 """
 
 import sqlite3
-
+conn = sqlite3.connect("tickets.db")
 
 def customer_tickets(conn, customer_id):
     """
@@ -18,7 +18,18 @@ def customer_tickets(conn, customer_id):
     Include only tickets purchased by the given customer_id.
     Order results by film title alphabetically.
     """
-    pass
+    
+
+    q = """SELECT f.title, s.screen, t.price
+    FROM films f JOIN screenings s ON f.film_id=s.film_id
+    JOIN tickets t ON t.screening_id=s.screening_id
+    WHERE t.customer_id=?
+    ORDER BY f.title;"""
+
+    cur = conn.execute(q, (customer_id,))
+
+    return cur.fetchall()
+    
 
 
 def screening_sales(conn):
@@ -29,7 +40,18 @@ def screening_sales(conn):
     Include all screenings, even if tickets_sold is 0.
     Order results by tickets_sold descending.
     """
-    pass
+
+    q = """SELECT s.screening_id, f.title, COUNT(t.ticket_id) AS t_sold
+    FROM screenings s LEFT JOIN tickets t ON s.screening_id=t.screening_id
+    JOIN films f ON f.film_id=s.film_id
+    GROUP BY s.screening_id
+    ORDER BY t_sold DESC;"""
+
+    cur = conn.execute(q)
+    return cur.fetchall()
+    
+
+
 
 
 def top_customers_by_spend(conn, limit):
@@ -42,4 +64,13 @@ def top_customers_by_spend(conn, limit):
     Order by total_spent descending.
     Limit the number of rows returned to `limit`.
     """
-    pass
+    
+    q = """SELECT c.customer_name, SUM(t.price) AS spent
+    FROM customers c LEFT JOIN tickets t ON c.customer_id=t.customer_id
+    GROUP BY c.customer_id HAVING COUNT(t.ticket_id) >= 1
+    ORDER BY spent DESC
+    LIMIT ?;
+    """
+
+    cur = conn.execute(q, (limit,))
+    return cur.fetchall()
